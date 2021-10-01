08b6c16f57f14fc7a52df46fc9eb184ddc475869
varnikak00 <varnikak7070@gmail.com>
2021-10-01 14:28:52 +0530

    genericdatatype

---
+++ GenericDatatype1/GenericType.cs
@@ -0,0 +1,47 @@
+﻿namespace GenericDatatype1
+{
+    internal class GenericType
+    {
+        public int FindMax(int num1, int num2, int num3)
+        {
+            if (num1 > num2 && num1 > num3)
+            {
+                return num1;
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                return num2;
+            }
+            else if (num3 > num2 && num3 > num1)
+            {
+                return num3;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+        public float FindMax(float num1, float num2, float num3)
+        {
+            if (num1 > num2 && num1 > num3)
+            {
+                return num1;
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                return num2;
+            }
+            else if (num3 > num2 && num3 > num1)
+            {
+                return num3;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+        
+    }
+}
---
+++ GenericDatatype1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericDatatype1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter three number");
+            float num1 = float.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+            float num3 = float.Parse(Console.ReadLine());
+            GenericType gt = new GenericType();
+            float maxNumber = gt.FindMax(num1, num2, num3);
+            Console.WriteLine("Max number is {0}", maxNumber);
+
+        }
+    }
+}
