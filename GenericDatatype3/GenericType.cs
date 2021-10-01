d2fd046eebb4f679505d3f8cff5c52e129198eec
varnikak00 <varnikak7070@gmail.com>
2021-10-01 14:31:27 +0530

    genericdatatype

---
+++ GenericDatatype3/GenericType.cs
@@ -0,0 +1,77 @@
+﻿namespace GenericDatatype3
+{
+    internal class GenericType
+    {
+        public int FindMax(int num1, int num2, int num3);
+        public static  int FindMax(int num1, int num2, int num3)
+        {
+            if (num1 > num2 && num1 > num3)
+            {
+                return num1;
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                return num2;
+            }
+            else if (num3 > num2 && num3 > num1)
+            {
+                return num3;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+        public float FindMax(float num1, float num2, float num3)
+        {
+            if (num1 > num2 && num1 > num3)
+            {
+                return num1;
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                return num2;
+            }
+            else if (num3 > num2 && num3 > num1)
+            {
+                return num3;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+
+        public string FindMax(string firstString, string secondString, string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0
+                )
+            {
+                return firstString;
+            }
+            else if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0
+                )
+            {
+                return secondString;
+            }
+            else if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) > 0 && secondString.CompareTo(secondString) >= 0
+                )
+            {
+                return thirdString;
+            }
+            else
+            {
+                return "all string are same";
+
+            }
+        }
+    }
+}
---
+++ GenericDatatype3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericDatatype3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Enter three number");
+            float num1 = float.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+            float num3 = float.Parse(Console.ReadLine());
+            GenericType gt = new GenericType();
+            float maxNumber = gt.FindMax(num1, num2, num3);
+            Console.WriteLine("Max number is {0}", maxNumber);
+            */
+            GenericType gt = new GenericType();
+            string maxString = gt.FindMax("Eleven", "thirtyThree", "fourtyFour");
+            Console.WriteLine("Max string is {0}", maxString);
+
+        }
+    }
+}
